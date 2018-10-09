badcd0082d1d8ef7673ee002fbac523b6d3e7248
David Saada <david.saada@arm.com>
2018-10-09 19:11:34 +0300

    KVStore & derived classes: design docs, implementation & configuration
    Implement the following:
    KVStore base class
    TDBStore class
    FileSystemStore class
    SecureStore class
    Global APIs
    Configuration framework
    Design documentation
    
    # Conflicts:
    #	targets/targets.json

--- features/storage/blockdevice/BufferedBlockDevice.cpp
+++ features/storage/blockdevice/BufferedBlockDevice.cpp
@@ -119,11 +119,14 @@ int BufferedBlockDevice::read(void *b, bd_addr_t addr, bd_size_t size)
 
     bd_addr_t aligned_addr = align_down(addr, _bd_program_size);
 
-    uint8_t *buf = static_cast<uint8_t *> (b);
+    uint8_t *buf = static_cast<uint8_t *>(b);
 
     if (aligned_addr != _curr_aligned_addr) {
         // Need to flush if moved to another program unit
-        flush();
+        int ret = flush();
+        if (ret) {
+            return ret;
+        }
         _curr_aligned_addr = aligned_addr;
         moved_unit = true;
     }
@@ -159,7 +162,7 @@ int BufferedBlockDevice::program(const void *b, bd_addr_t addr, bd_size_t size)
 
     bd_addr_t aligned_addr = align_down(addr, _bd_program_size);
 
-    const uint8_t *buf = static_cast <const uint8_t *> (b);
+    const uint8_t *buf = static_cast <const uint8_t *>(b);
 
     // Need to flush if moved to another program unit
     if (aligned_addr != _curr_aligned_addr) {
