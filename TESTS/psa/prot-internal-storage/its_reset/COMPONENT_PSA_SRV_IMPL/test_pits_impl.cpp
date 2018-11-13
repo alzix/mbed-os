5d1c6cd508eaa23dea892d91b5f84fa67cee0b4d
Danny Shavit <danny.shavit@arm.com>
2018-11-13 16:31:46 +0200

    Replace nvstore with kvstore in new write-once test

--- TESTS/psa/prot-internal-storage/its_reset/COMPONENT_PSA_SRV_IMPL/test_pits_impl.cpp
+++ TESTS/psa/prot-internal-storage/its_reset/COMPONENT_PSA_SRV_IMPL/test_pits_impl.cpp
@@ -2,19 +2,34 @@
 #include <stdlib.h>
 #include "psa_prot_internal_storage.h"
 #include "test_pits_impl.h"
-#include "nvstore.h"
+#include "kv_config.h"
+#include "KVMap.h"
+#include "KVStore.h"
+#include "mbed_error.h"
 
 #ifdef   __cplusplus
 extern "C"
 {
 #endif
 
+using namespace mbed;
+
 psa_its_status_t test_psa_its_reset_impl(void)
 {
     psa_its_status_t status = PSA_ITS_SUCCESS;
-    NVStore &nvstore = NVStore::get_instance();
 
-    if (nvstore.reset() != NVSTORE_SUCCESS) {
+    int kv_status = kv_init_storage_config();
+    if(kv_status != MBED_SUCCESS) {
+        return PSA_ITS_ERROR_STORAGE_FAILURE;
+    }
+
+    KVMap &kv_map = KVMap::get_instance();
+    KVStore *kvstore = kv_map.get_main_kv_instance("/kv/");//MBED_CONF_STORAGE_DEFAULT_KV); // Danny-TODO: Use configuration name
+    if (!kvstore) {
+        return PSA_ITS_ERROR_STORAGE_FAILURE;
+    }
+
+    if (kvstore->reset() != MBED_SUCCESS) {
         status = PSA_ITS_ERROR_STORAGE_FAILURE;
     }
 
