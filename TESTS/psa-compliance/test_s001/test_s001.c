9103ce5e76c356d8b79a613bd6a1de021cbe87ab
Oren Cohen <oren.cohen@arm.com>
2019-01-08 15:14:52 +0200

    ITS test patch 1

--- TESTS/psa-compliance/test_s001/test_s001.c
+++ TESTS/psa-compliance/test_s001/test_s001.c
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 **/
-
+#define ITS_TEST
 #include "val_interfaces.h"
 #include "val_target.h"
 #include "test_s001.h"
