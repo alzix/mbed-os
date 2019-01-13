cf746c9b502923cd8b5632679d6cf937760929bd
Michael Schwarcz <michael.schwarcz@arm.com>
2019-01-13 15:43:44 +0200

    DO_NOT_MERGE: Add build script

---
+++ tools/build_tfm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+MBED_OS_ROOT=$(dirname "$SCRIPT_DIR")
+TOOLCHAIN=$1
+shift
+
+mbed compile -t $TOOLCHAIN -m ARM_MUSCA_A1_S \
+    --source $MBED_OS_ROOT/components/TARGET_PSA \
+    --source $MBED_OS_ROOT/drivers \
+    --source $MBED_OS_ROOT/platform \
+    --source $MBED_OS_ROOT/cmsis \
+    --source $MBED_OS_ROOT/hal \
+    --source $MBED_OS_ROOT/targets \
+    --source $MBED_OS_ROOT/features/storage/blockdevice \
+    --source $MBED_OS_ROOT/components/storage/blockdevice \
+    --source $MBED_OS_ROOT/features/storage/kvstore/tdbstore \
+    --source $MBED_OS_ROOT/features/storage/kvstore/include \
+    --source $MBED_OS_ROOT/features/storage/kvstore/conf/tdb_internal \
+    --source $MBED_OS_ROOT/features/mbedtls \
+    $*
+
+cp BUILD/ARM_MUSCA_A1_S/$TOOLCHAIN/TARGET_PSA.bin $MBED_OS_ROOT/targets/TARGET_ARM_SSG/TARGET_MUSCA_A1/TARGET_MUSCA_A1_NS/device/tfm.bin
+cp BUILD/ARM_MUSCA_A1_S/$TOOLCHAIN/cmse_lib.o $MBED_OS_ROOT/targets/TARGET_ARM_SSG/TARGET_MUSCA_A1/TARGET_MUSCA_A1_NS/device/cmse_lib.o
