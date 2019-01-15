b3e2aed06ee55c98d357dd5278dfed69264fb2df
Oren Cohen <oren.cohen@arm.com>
2019-01-15 14:53:40 +0200

    2nd Patch

--- components/TARGET_PSA/TARGET_TFM/COMPONENT_SPE/secure_fw/core/ipc/include/tfm_spm_signal_defs.h
+++ components/TARGET_PSA/TARGET_TFM/COMPONENT_SPE/secure_fw/core/ipc/include/tfm_spm_signal_defs.h
@@ -6,9 +6,4 @@
  */
 #ifndef __TFM_SPM_SIGNAL_DEFS_H__
 #define __TFM_SPM_SIGNAL_DEFS_H__
-
-#include "test/test_services/tfm_ipc_service/tfm_ipc_service_partition.h"
-#include "secure_fw/services/secure_storage/tfm_sst_signal.h"
-#include "test/test_services/tfm_sst_test_service/tfm_sst_test_service_signal.h"
-
 #endif
