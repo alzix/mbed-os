8b9b0b2fcebae6be792b21964802adca6abf06e8
Michael Schwarcz <michael.schwarcz@arm.com>
2019-01-15 14:13:04 +0200

    Implement flash hal api

--- targets/TARGET_ARM_SSG/TARGET_MUSCA_A1/COMPONENT_SPE/flash_api.c
+++ targets/TARGET_ARM_SSG/TARGET_MUSCA_A1/COMPONENT_SPE/flash_api.c
@@ -15,25 +15,54 @@
  */
 
 #include "device.h"
+#include "flash_layout.h"
 #include "flash_api.h"
 
 #if DEVICE_FLASH
 
+#define FLASH_DEV          MT25QL_DEV_NS
+
 int32_t flash_init(flash_t *obj)
 {
     (void)(obj);
+    enum mt25ql_error_t err = MT25QL_ERR_NONE;
+
+    qspi_ip6514e_enable(FLASH_DEV.controller);
+
+    /* Configure QSPI Flash controller to operate in single SPI mode and
+     * to use fast Flash commands */
+    err = mt25ql_config_mode(&FLASH_DEV, MT25QL_FUNC_STATE_FAST);
+    if (err != MT25QL_ERR_NONE) {
+        return -1;
+    }
+
     return 0;
 }
 
 int32_t flash_free(flash_t *obj)
 {
     (void)(obj);
+    enum mt25ql_error_t err = MT25QL_ERR_NONE;
+
+    /* Restores the QSPI Flash controller and MT25QL to default state */
+    err = mt25ql_restore_default_state(&FLASH_DEV);
+    if (err != MT25QL_ERR_NONE) {
+        return -1;
+    }
+
     return 0;
 }
 
 int32_t flash_erase_sector(flash_t *obj, uint32_t address)
 {
     (void)(obj);
+    enum mt25ql_error_t err = MT25QL_ERR_NONE;
+
+    err = mt25ql_erase(&FLASH_DEV, address, MT25QL_ERASE_SUBSECTOR_4K);
+    if (err != MT25QL_ERR_NONE) {
+        return -1;
+    }
+
     return 0;
 }
 
@@ -41,43 +70,61 @@ int32_t flash_read(flash_t *obj, uint32_t address,
                    uint8_t *data, uint32_t size)
 {
     (void)obj;
+    enum mt25ql_error_t err = MT25QL_ERR_NONE;
+
+    err = mt25ql_command_read(&FLASH_DEV, address, data, size);
+    if (err != MT25QL_ERR_NONE) {
+        return -1;
+    }
+
     return 0;
 }
 
 int32_t flash_program_page(flash_t *obj, uint32_t address, const uint8_t *data, uint32_t size)
 {
     (void)(obj);
+    enum mt25ql_error_t err = MT25QL_ERR_NONE;
+
+    err = mt25ql_command_write(&FLASH_DEV, address, data, size);
+    if (err != MT25QL_ERR_NONE) {
+        return -1;
+    }
+
     return 0;
 }
 
 uint32_t flash_get_sector_size(const flash_t *obj, uint32_t address)
 {
     (void)(obj);
-    return 0;
+    if ((address >= NS_QSPI_ALIAS_BASE) && (address < NS_QSPI_ALIAS_BASE + FLASH_TOTAL_SIZE)) {
+        return SUBSECTOR_4KB;
+    }
+
+    return MBED_FLASH_INVALID_SIZE;
 }
 
 uint32_t flash_get_page_size(const flash_t *obj)
 {
     (void)(obj);
-    return 0;
+    return FLASH_PAGE_SIZE;
 }
 
 uint32_t flash_get_start_address(const flash_t *obj)
 {
     (void)(obj);
-    return 0;
+    return NS_QSPI_ALIAS_BASE;
 }
 
 uint32_t flash_get_size(const flash_t *obj)
 {
     (void)(obj);
-    return 0;
+    return FLASH_TOTAL_SIZE;
 }
 
 uint8_t flash_get_erase_value(const flash_t *obj)
 {
     (void)obj;
-    return 0x00;
+    return 0xFF;
 }
 
 #endif // DEVICE_FLASH
