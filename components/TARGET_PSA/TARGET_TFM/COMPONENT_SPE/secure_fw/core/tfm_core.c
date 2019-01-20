3d52d84df0bc3ff570666f10943bdd5644a8c66b
Oren Cohen <oren.cohen@arm.com>
2019-01-20 14:46:51 +0200

    [trusted-firmware-m]: Updated to 0fa8aaa1

--- components/TARGET_PSA/TARGET_TFM/COMPONENT_SPE/secure_fw/core/tfm_core.c
+++ components/TARGET_PSA/TARGET_TFM/COMPONENT_SPE/secure_fw/core/tfm_core.c
@@ -11,9 +11,18 @@
 #include "tfm_internal.h"
 #include "tfm_api.h"
 #include "platform/include/tfm_spm_hal.h"
+#include "uart_stdout.h"
 #include "secure_utilities.h"
 #include "secure_fw/spm/spm_api.h"
 #include "secure_fw/include/tfm_spm_services_api.h"
+#ifdef TFM_PSA_API
+#include "psa_client.h"
+#include "psa_service.h"
+#include "tfm_thread.h"
+#include "tfm_wait.h"
+#include "tfm_message_queue.h"
+#include "tfm_spm.h"
+#endif
 
 /*
  * Avoids the semihosting issue
@@ -47,53 +56,6 @@ __asm("  .global __ARM_use_no_argv\n");
 REGION_DECLARE(Image$$, TFM_UNPRIV_SCRATCH, $$ZI$$Base);
 REGION_DECLARE(Image$$, TFM_UNPRIV_SCRATCH, $$ZI$$Limit);
 
-#define SPNIDEN_SEL_STATUS (0x01u << 7)
-#define SPNIDEN_STATUS     (0x01u << 6)
-#define SPIDEN_SEL_STATUS  (0x01u << 5)
-#define SPIDEN_STATUS      (0x01u << 4)
-#define NIDEN_SEL_STATUS   (0x01u << 3)
-#define NIDEN_STATUS       (0x01u << 2)
-#define DBGEN_SEL_STATUS   (0x01u << 1)
-#define DBGEN_STATUS       (0x01u << 0)
-
-#define All_SEL_STATUS (SPNIDEN_SEL_STATUS | SPIDEN_SEL_STATUS | \
-                        NIDEN_SEL_STATUS | DBGEN_SEL_STATUS)
-
-void configure_debug_registers(void)
-{
-    volatile struct sysctrl_t *sys_ctrl =
-        (struct sysctrl_t *)CMSDK_SYSCTRL_BASE_S;
-
-#if defined(DAUTH_NONE)
-    /* Set all the debug enable selector bits to 1 */
-    sys_ctrl->secdbgset = All_SEL_STATUS;
-    /* Set all the debug enable bits to 0 */
-    sys_ctrl->secdbgclr =
-        DBGEN_STATUS | NIDEN_STATUS | SPIDEN_STATUS | SPNIDEN_STATUS;
-#elif defined(DAUTH_NS_ONLY)
-    /* Set all the debug enable selector bits to 1 */
-    sys_ctrl->secdbgset = All_SEL_STATUS;
-    /* Set the debug enable bits to 1 for NS, and 0 for S mode */
-    sys_ctrl->secdbgset = DBGEN_STATUS | NIDEN_STATUS;
-    sys_ctrl->secdbgclr = SPIDEN_STATUS | SPNIDEN_STATUS;
-#elif defined(DAUTH_FULL)
-    /* Set all the debug enable selector bits to 1 */
-    sys_ctrl->secdbgset = All_SEL_STATUS;
-    /* Set all the debug enable bits to 1 */
-    sys_ctrl->secdbgset =
-        DBGEN_STATUS | NIDEN_STATUS | SPIDEN_STATUS | SPNIDEN_STATUS;
-#else
-#if !defined(DAUTH_CHIP_DEFAULT)
-#error "No debug authentication setting is provided."
-#endif
-    /* Set all the debug enable selector bits to 0 */
-    sys_ctrl->secdbgclr = All_SEL_STATUS;
-    /* No need to set any enable bits because the value depends on
-     *input signals
-     */
-#endif
-}
-
 void configure_ns_code(void)
 {
     /* SCB_NS.VTOR points to the Non-secure vector table base address */
@@ -121,16 +83,19 @@ int32_t tfm_core_init(void)
     /* Configures the system reset request properties */
     system_reset_cfg();
 
-    /* Configure the debug configuration registers */
-    configure_debug_registers();
+    /* Configures debug authentication */
+    tfm_spm_hal_init_debug();
 
     __enable_irq();
 
+    stdio_init();
     LOG_MSG("Secure image initializing!");
 
 #ifdef TFM_CORE_DEBUG
     printf("TFM level is: %d\r\n", TFM_LVL);
 #endif
+
+    tfm_core_validate_boot_data();
 
     tfm_spm_hal_init_isolation_hw();
 
@@ -197,15 +162,20 @@ int main(void)
 
     tfm_spm_partition_set_state(TFM_SP_CORE_ID, SPM_PARTITION_STATE_RUNNING);
 
-    extern uint32_t Stack_Mem[];
+    extern uint32_t Image$$ARM_LIB_STACK$$ZI$$Base[];
+    uint32_t psp_stack_bottom = (uint32_t)Image$$ARM_LIB_STACK$$ZI$$Base;
 
-    __set_PSPLIM((uint32_t)Stack_Mem);
+    __set_PSPLIM(psp_stack_bottom);
 
     if (tfm_spm_partition_init() != SPM_ERR_OK) {
         /* Certain systems might refuse to boot altogether if partitions fail
          * to initialize. This is a placeholder for such an error handler
          */
     }
+
+#ifdef TFM_PSA_API
+    tfm_spm_init();
+#endif
 
 #ifdef TFM_CORE_DEBUG
     /* Jumps to non-secure code */
